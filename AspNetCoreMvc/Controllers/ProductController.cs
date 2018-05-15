014f6873c058a87d47a3203ae23e083e7a2c2c31
Skylar Carter <carter.skylar@gmail.com>
2018-05-15 09:05:40 -0500

    Added Product Controllere and View

--- AspNetCoreMvc/Controllers/ProductController.cs
+++ AspNetCoreMvc/Controllers/ProductController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using itb.Shared;
 
 namespace AspNetCoreMvc.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductRepository _prodRepo;
+
+        public ProductController(IProductRepository prodRepo)
         {
-            return View();
+            _prodRepo = prodRepo;
+        }
+
+        public IActionResult Index()
+
+        {
+            var products = _prodRepo.GetProducts();
+            return View(products);
         }
     }
 }
